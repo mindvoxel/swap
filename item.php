e6aa0941b35d0ba4decdae2742e6fe6414ee6b11
christopher-wolff23 <cwolff12@umd.edu>
2018-10-11 23:17:23 -0400

    filled out the item.php class

--- item.php
+++ item.php
@@ -1,5 +1,44 @@
 <?php
 
+    /*Items will be stored in an array in user's profile. This is the 
+    object that users will be trading back and forth.*/
+    class Item{
+
+        private $name;
+        private $description; //information about the item
+        private $value; //estimated value of the item 
+                        //potentially have a field for image hyperlink?
+
+        //Would be nice to always have items with a name, description passed in can be
+        //empty, I suppose. Value should always be provided (by the user?)
+        public function __constructor(string $name, string $description,
+        float $value){
+            $this->name = $name;
+            $this->description = $description;
+            $this->value = $value;
+        }
+
+        //getter
+        public function get_name() :string{
+            return $this->name;
+        }
+
+        //getter
+        public function get_description() :string{
+            return $this->description;
+        }
+
+        //getter
+        public function get_value() :float{
+            return $this->float;
+        }
+
+        public function __toString(){
+            return "name: " . $this->name . " description: " . $this->password .
+            " value: " . $this->value;
+        }
+
+    }
 
 
 ?>
--- profile.php
+++ profile.php
@@ -2,7 +2,7 @@
     /*The data from this class will be placed in an sql database. The
     data will be generated from a form on the front-end. It represents
     a user's swap profile. This class can be used to format the 
-    data on initialialization, ensure that the data fits certain criteria, 
+    data on initialization, ensure that the data fits certain criteria, 
     and do null checks*/
 
     class Profile{
@@ -10,15 +10,31 @@
                            //that can also be done on the front end
         private $password; //same
         private $inventory; //should be a list of 'items'
+                            //the size of this item will be the 
+                            //number of items in a user's inventory
 
-        //Dont allow the feilds to be null. Rather provide default values.
-        public function __constructor(){
-
+        //Dont allow the feilds to be null. Profile should always have atleast
+        //a username, a password, and an inventory
+        public function __constructor(string $username, string $password,
+        array $inventory){
+            $this->username = $username;
+            $this->password = $password;
+            $this->inventory = $inventory;
         }
 
         //getter
         public function get_username() :string{
+            return $this->username;
+        }
+
+        //getter
+        public function get_password() :string{
             return $this->password;
+        }
+
+        //getter
+        public function get_inventory() :array{
+            return $this->inventory;
         }
 
         //to string function
