d2f26bd2c7bf5ac0950b82062dabbe9264d93135
christopher-wolff23 <cwolff12@umd.edu>
2018-11-22 23:53:54 -0500

    added js validation page/template

---
+++ validation.js
@@ -0,0 +1,11 @@
+/*This page will perform client side validation for all parts of the website. Any page
+that needs JS validation will run this script*/
+
+
+//register event listeners
+main = function(){
+    file = getElementByID();
+}
+
+main();
+
